a6c238e86944adbf3e7f79fd2f1aad61b315be4c
naglismoc <dvd.filmas@gmail.com>
2023-09-22 11:08:13 +0300

    java functions sunkesniu antras

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        int[] rndIntArr = rndIntArr(4,10,6);
-        printIntArr(rndIntArr);
-        System.out.println(sumIntArr(rndIntArr));
-        System.out.println(avgIntArr(rndIntArr));
+        String rndStr = generateRndStr(10);
+        System.out.println(rndStr);
+        printStrInAWeirdWay(rndStr);
     }
 /*
-4. Sukurkite Funkciją kuri priima int[] tipo kintamąį, prasuka ciklą ir atspausdina kiekvieną skaičių.
-5. Sukurkite Funkciją kuri priima du int tipo kintamuosius, min ir max reikšmėms nustatyti ir sugeneruoja random int skaičių ir jį gražintų.
-6. Sukurkite Funkciją kuri sugeneruotų random int skaičių masyvą ir jį gražintų. Funkcija priima tris int tipo kintamuosius, min, max ir length reikšmėms nustatyti.
-7. Sukurkite Funkciją kuri panaudotų 6tos užduoties masyvą (priimtų kaip kintamąjį), susumuotų ir gražintų reikšmę.
-8. Sukurkite Funkciją kuri priimtų 6tos užduoties masyvą ir gražintų jos skaičių vidurkį (double).
-
+Sugeneruokite atsitiktinį stringą iš raidžių ir skaičių (10 simbolių). Atspausdinkite simbolius stulpeliu.
+Jei tai skaičius apgaubkite “ [ 7 ]”. Jei skaičiai eina keli iš eilės, apgaubkite juos kartu. [75].
  */
-    public static int rndInt (int min, int max){
-        return min + (int) Math.round(Math.random() * (max - min));
+
+
+    public static void printStrInAWeirdWay(String str){
+        String tempStr = "";//16
+        for (int i = 0; i < str.length(); i++) {
+            if(Character.isDigit(str.charAt(i))){
+             tempStr += str.charAt(i);
+            }else {
+                if(!tempStr.isEmpty()){
+                    System.out.println("[" + tempStr + "]");
+                    tempStr = "";
+                }
+                System.out.println(str.charAt(i));
+            }
+        }
+        if(!tempStr.isEmpty()){
+            System.out.println("[" + tempStr + "]");
+        }
     }
 
-    public static int[] rndIntArr(int min, int max, int len){
-        int[] arr = new int[len];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = rndInt(min, max);
+    public static String generateRndStr(int length) {
+        String symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678901234567890";
+        String text = "";
+        for (int i = 0; i < length; i++) {
+            text += symbols.charAt((int) (Math.random()*symbols.length()));
         }
-        return arr;
+        return text;
     }
 
-    public static void printIntArr(int[] arr){
-        for (int number : arr) {
-            System.out.print(number + " ");
-        }
-        System.out.println();
-    }
-    public static int sumIntArr(int[] arr){
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-        }
-        return sum;
-    }
-    public static double avgIntArr(int[] arr){
-        return (double) sumIntArr(arr) / arr.length;
-    }
 }
